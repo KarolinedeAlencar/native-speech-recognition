a70ea952df1db78b3099aaaf3156463ea117bdb7
KarolinedeAlencar <karolinedalencar@gmail.com>
2017-10-06 07:44:41 -0300

    styling and add eastereggs

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ recognition.interimResults = true;
 recognition.lang = 'en-US';
 
 let p = document.createElement('p');
-const words = document.querySelector('.words');
+const words = document.querySelector('[data-js="words"]');
 words.appendChild(p);
 
 recognition.addEventListener('result', e => {
@@ -23,8 +23,19 @@ recognition.addEventListener('result', e => {
     }
 
     if(transcript.includes('dog')) {
-      const img = `<img src="https://i.giphy.com/media/mokQK7oyiR8Sk/giphy.webp" alt="Cute doggos"/>`;
-      words.innerHTML = img;
+      const gif = `
+        <h2 class="easteregg__title">Dogs are awesome! <3</h2>
+        <img class="easteregg__img" src="https://i.giphy.com/media/mokQK7oyiR8Sk/giphy.webp" alt="Cute doggos"/>
+      `;
+      words.innerHTML = gif;
+    }
+
+    if(transcript.includes('cat')) {
+      const gif = `
+        <h2 class="easteregg__title">Cats are awesome! <3</h2>
+        <img class="easteregg__img" src="https://heavyeditorial.files.wordpress.com/2012/08/roomba-cat-vs-dog.gif?w=780" alt="Cute doggos"/>
+      `;
+      words.innerHTML = gif;
     }
 
 
